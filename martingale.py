5fa87720624c823f2a059828881067d77a8cf865
Aleksey Manin <alkor135@gmail.com>
2020-05-04 17:26:19 +0300

    martingale

--- martingale.py
+++ martingale.py
@@ -1,17 +1,21 @@
 import random
 
-depo = 10000
+depo = 100000
 profit = 0
 max_bet = 0
 min_profit = 0
-#max_iter = 0
-for i in range(10000):
-    if profit >= 0:
-        bet = 1
-        profit = 0
-        #max_iter = 1
-    else:
-        bet = bet * 2
+iter = 0
+max_iter = 0
+for i in range(1000000):  # Количество ставок
+    if profit >= 0:  # Если профит от предыдущей сделки положительный
+        bet = 1  # Ставку устанавливаем в 1
+        profit = 0  # Профит обнуляем
+        iter = 0  # Обнуляем количество ставок
+    else:  # Если профит от предыдущей сделки отрицательный
+        iter += 1  # Увеличиваем счетчик иттераций на 1 для подсчета колен
+        bet = bet * 2  # Увеличиваем ставку в 2 раза
+        if max_iter < iter:  # Если максимальное количество иттераций меньше чем текущее кол-во иттераций
+            max_iter = iter  # Записываем новое количество иттераций, для подсчета макс кол-ва колен
     if bet > max_bet:
         max_bet = bet
     bet_int = random.randint(0, 1)
@@ -25,8 +29,10 @@ for i in range(10000):
 
     if profit < min_profit:
         min_profit = profit
-    print('bet =', bet)
-    print('profit =', profit)
+    #print('bet =', bet)
+    #print('profit =', profit)
+    #print(max_iter)
 print(depo)
 print('max_bet =', max_bet)
 print('min_profit =', min_profit)
+print('max_iter =', max_iter)
