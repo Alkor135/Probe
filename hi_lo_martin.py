30ce668d498be5121d6caaf5d6c74f32ff3dfd21
Alkor135 <alkor135@gmail.com>
2020-05-08 19:32:45 +0300

    new

---
+++ hi_lo_martin.py
@@ -0,0 +1,37 @@
+import random
+
+
+min_depo = 10000.0
+for _ in range(1000):
+    if min_depo < 0:
+        break
+    start_depo = 10000.0  # Старт Депо
+    stop_depo = start_depo + start_depo * 0.0003  # Стоп Депо
+    bet_percent = 0.0001  # Процент ставки от депо
+    bet = start_depo * bet_percent  # Первоначальная ставка
+    step_percent = 0.25
+    # print(start_depo)
+    # print(bet)
+    # print(stop_depo)
+    depo = start_depo
+    plus = 0
+    minus = 0
+    while True:
+        if min_depo > depo:
+            min_depo = depo
+        if stop_depo < depo or depo < 0.0:
+            break
+        bet_iter = random.randint(0, 99)
+        if bet_iter > 50:
+            depo = depo + bet
+            bet = bet - bet * step_percent
+            plus += 1
+        else:
+            depo = depo - bet
+            bet = bet + bet * step_percent
+            minus += 1
+
+    print(plus)
+    print(minus)
+    print(depo)
+print(min_depo)
