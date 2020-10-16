e734caf27e186f2c7bc0abf8cf01e8d7c6d7eaa7
Alkor135 <alkor135@gmail.com>
2020-10-17 00:13:30 +0300

    finam

--- finam_downloader/down_finam.py
+++ finam_downloader/down_finam.py
@@ -7,22 +7,19 @@ from urllib.request import urlopen
 from datetime import datetime
 from pathlib import Path
 from settings import *
+import pandas as pd
+import time
 
 
 class DownloadFinam:
-    def __init__(self, ticker, start, end, period=3):
+    def __init__(self, ticker, period=3):
         self.ticker = ticker
         self.period = period
-        self.start = start
-        self.end = end
         self.market = 14  # Для фьючерсов
         self.datf = 7  # Для тиков
-        self.start_date_rev = 0
-        self.end_date_rev = 0
         self.url = ''
-        self.file_path = 0
 
-    def url_finam(self):
+    def url_finam(self, date_rev):
         """
         Функция составляет url для запроса на сервер FINAMa
         """
@@ -36,11 +33,8 @@ class DownloadFinam:
         else:
             self.datf = 1  # Формат записи для минутных баров 'TICKER, PER, DATE, TIME, OPEN, HIGH, LOW, CLOSE, VOL'
 
-        # Делаем преобразования дат:
-        start_date = datetime.strptime(self.start, "%d.%m.%Y").date()  # Переводим формат в datetime
-        self.start_date_rev = datetime.strptime(self.start, '%d.%m.%Y').strftime('%Y%m%d')  # Дата в нужном формате строкой
-        end_date = datetime.strptime(self.end, "%d.%m.%Y").date()
-        self.end_date_rev = datetime.strptime(self.end, '%d.%m.%Y').strftime('%Y%m%d')
+        start_date = datetime.strptime(date_rev, "%Y%m%d").date()  # Переводим формат в datetime
+        end_date = start_date
 
         # Все параметры упаковываем в единую структуру.
         # Здесь есть дополнительные параметры, кроме тех, которые заданы в шапке. См. комментарии.
@@ -58,7 +52,7 @@ class DownloadFinam:
             ('yt', end_date.year),  # Конечная дата, год
             ('to', end_date),  # Конечная дата
             ('p', self.period),  # Таймфрейм
-            ('f', self.ticker + "_" + self.start_date_rev + "_" + self.end_date_rev),  # Имя сформированного файла
+            ('f', self.ticker + "_" + date_rev + "_" + date_rev),  # Имя сформированного файла
             ('e', ".csv"),  # Расширение сформированного файла
             ('cn', self.ticker),  # ещё раз тикер
             # См.страницу  # https://www.finam.ru/profile/moex-akcii/sberbank/export/
@@ -73,44 +67,53 @@ class DownloadFinam:
             ('at', 1)  # Нужны ли заголовки столбцов
         ])
 
-        self.url = f'{FINAM_URL}{ticker}_{self.start_date_rev}_{self.end_date_rev}.csv?{params}'  # урл составлен!
+        self.url = f'{FINAM_URL}{ticker}_{date_rev}_{date_rev}.csv?{params}'  # урл составлен!
         # print(f'Ссылка для запроса готова: {url}')
 
-    def path(self):
+    def path(self, date_rev):
         """
         Функция создает имя файла и путь его сохранения
         """
         period_txt = PERIODS[period]  # Для добавления к имени файла периода
-        file_name = f'{self.ticker}_{self.start_date_rev}_{self.end_date_rev}_{period_txt}.csv'  # Имя выходного файла
+        file_name = f'{self.ticker}_{date_rev}_{date_rev}_{period_txt}.csv'  # Имя выходного файла
         # print(f'Имя выходного файла {file_name}')
 
         dir_path = Path('..') / 'data_finam'  # Папка для сохранения (родительский каталог, папка data_finam)
         if not dir_path.exists():  # Проверяем существует ли папка
             dir_path.mkdir()  # Создаем папку при её отсутствии
 
-        self.file_path = Path('..') / 'data_finam' / file_name  # Создаем пути для сохранения файла
+        return Path('..') / 'data_finam' / file_name  # Создаем пути для сохранения файла
 
-    def download(self):
-        if not self.file_path.exists():  # Если файла не существует
+    def download(self, date_rev):
+        self.url_finam(date_rev)  # Вызываем функцию составления url
+        file_path = self.path(date_rev)  # Вызываем функцию составления путей и имени файла
+        if not file_path.exists():  # Если файла не существует
             txt = urlopen(self.url).readlines()  # Получаем в txt массив данных с Финама.
 
-            with open(self.file_path, 'w', encoding='utf-8') as file_out:  # задаём файл, в который запишем котировки.
+            with open(file_path, 'w', encoding='utf-8') as file_out:  # задаём файл, в который запишем котировки.
                 for line in txt:  # записываем файл строку за строкой.
                     file_out.write(line.strip().decode("utf-8") + '\n')
 
-            print(f'Готово. Проверьте файл {str(self.file_path)} по указанному пути')
+            print(f'Готово. Проверьте файл {file_path} по указанному пути')
         else:
-            print(f'Файл {str(self.file_path)} уже существует')
+            print(f'Файл {file_path} уже существует')
 
 
 if __name__ == "__main__":
 
     ticker = "SPFB.RTS"  # задаём тикер
-    period = 1  # задаём период. Выбор из: 'tick': 1, 'min': 2, '5min': 3, '10min': 4, '15min': 5, '30min': 6, 'hour': 7, 'daily': 8, 'week': 9, 'month': 10
-    start = "02.09.2020"  # с какой даты начинать тянуть котировки
-    end = "02.09.2020"  # финальная дата, по которую тянуть котировки
+    period = 3  # задаём период. Выбор из: 'tick': 1, 'min': 2, '5min': 3, '10min': 4, '15min': 5, '30min': 6, 'hour': 7
+    start = "01.01.2020"  # с какой даты начинать тянуть котировки
+    end = "01.09.2020"  # финальная дата, по которую тянуть котировки
 
-    data = DownloadFinam(ticker, start, end, period)
-    data.url_finam()
-    data.path()
-    data.download()
+    # Делаем преобразования дат:
+    start_date_range = datetime.strptime(start, '%d.%m.%Y').strftime('%Y%m%d')  # Дата в нужном формате строкой
+    end_date_range = datetime.strptime(end, '%d.%m.%Y').strftime('%Y%m%d')
+    date_range = pd.date_range(start_date_range, end_date_range)
+
+    data = DownloadFinam(ticker, period)  # Создаем экземпляр класса
+    for single_date in date_range:
+        download_date = single_date.strftime('%Y%m%d')
+        data.download(download_date)
+
+        time.sleep(3)  # Сон в 3 секунды
