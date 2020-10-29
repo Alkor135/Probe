e686dcceea56eb0f11a58e9be6191d55e8f3d6bb
Aleksey Manin <alkor135@gmail.com>
2020-10-29 10:23:25 +0300

    descript.ion

--- descript_ion/descript_ion_encoding.py
+++ descript_ion/descript_ion_encoding.py
@@ -9,8 +9,22 @@ if __name__ == "__main__":
     # print(file_lst)
 
     print(pathlib.PurePath('c:/Program Files/').root)
-    print(pathlib.PurePath(pathlib.Path.cwd()).root)
+    print(pathlib.PurePath('c:/').root)
 
-    path = pathlib.Path('c:/')
-    file_lst = list(path.glob('**/descript.ion'))
-    print(file_lst)
+    # path = pathlib.Path('c:/')
+    # file_lst = list(path.glob('**/descript.ion'))  # Создает список всех файлов descript.ion на диске с
+    # print(file_lst)
+
+    path = pathlib.Path('c:/descript.ion')  # Путь к файлу который читаем
+
+    path_out = pathlib.Path('c:/descript_ion.new')  # Путь к файлу который создаем
+    # path_out.touch(mode=0o666, exist_ok=True)  # Отказано в доступе
+
+    text_file = path.read_text(encoding='cp866')  # Чтение файла
+
+    # path_out.write_text(text_file, encoding='UTF8')
+
+    with path_out.open(mode='wt') as new_file:  # Отказано в доступе
+        new_file.write(text_file)
+
+    print(text_file)
