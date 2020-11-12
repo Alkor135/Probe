042b1d61f8d36ef8f437befe03ecd7783dcd69e8
Aleksey Manin <Alkor135@gmail.com>
2020-11-12 17:03:00 +0300

    Чистка

--- finam_downloader/csv_to_df.py
+++ finam_downloader/csv_to_df.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
 
-import pandas as pd
-
-df_data = pd.read_csv('../data_finam/SPFB.RTS_20200103_20200103_5min.csv')  # Загрузка данных их файла в df
-print(df_data)
-
-df_data['date_time'] = df_data['<DATE>'].astype(str) + ' ' + df_data['<TIME>'].astype(str)  # Слияние столбцов
-print(df_data)
-print(df_data.columns.values)  # Проверяем, что есть в названиях столбцов
+# import pandas as pd
+from pathlib import Path
 
 
+if __name__ == "__main__":
+
+	# Выбор файла с ТФ в минутах
+	dir_name = 'c:/data_finam_quote_csv'  # Папка с файлами
+	file_name = 'SPFB.RTS_5min_200103.csv'  # Имя файла
+	file_path = Path(f'{dir_name}/{file_name}')  # Путь к файлу
+	if file_path.exists():  # Если файл существует
+		split_name = file_name.split('_')
+		print(split_name)
+
+
+	df_data = pd.read_csv(file_path)  # Загрузка данных их файла в df
+	print(df_data)
+
+	df_data['date_time'] = df_data['<DATE>'].astype(str) + ' ' + df_data['<TIME>'].astype(str)  # Слияние столбцов
+	print(df_data)
+	print(df_data.columns.values)  # Проверяем, что есть в названиях столбцов
